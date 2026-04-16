711db2c76425b1ce0a8da703811d251dc3d1f96c
calirko <me@calirko.com>
2026-04-16 12:43:41 -0300

    stuff

---
+++ apps/server/src/scheduler.ts
@@ -0,0 +1,6 @@
+const schedules = new Map<string, NodeJS.Timeout>();
+
+export async function Scheduler() {
+  // when initialised, will run every minute and check if there are any backup jobs that need to be run
+
+}
